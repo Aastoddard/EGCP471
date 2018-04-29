c3753fe4735daf48af69ab69d643d8a60c1453c3
Aastoddard <aaron.a.stoddard@gmail.com>
2018-04-29 12:47:49 -0700

    Add files via upload

---
+++ Bluetooth/Nano_Analog_BT.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h> //Serial library
+
+SoftwareSerial bt (0,1);  //RX, TX (Switched on the Bluetooth - RX -> TX | TX -> RX)
+float fsr = 0;
+int inputPin = A5;
+int btdata; // the data given from the computer
+ 
+void setup() {
+  bt.begin(9600);
+  pinMode(inputPin, INPUT);
+}
+ 
+void loop() {
+  fsr = analogRead(inputPin);
+  
+  bt.print(fsr);
+  bt.print("\n");
+  
+  delay (2000); //prepare for data (2s)
+}
