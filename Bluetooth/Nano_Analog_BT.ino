9c600b80bac5cf7abe892da88b0a778da917a5c5
Aastoddard <aaron.a.stoddard@gmail.com>
2018-04-29 12:50:07 -0700

    Update Nano_Analog_BT.ino

--- Bluetooth/Nano_Analog_BT.ino
+++ Bluetooth/Nano_Analog_BT.ino
@@ -1,12 +1,13 @@
 #include <SoftwareSerial.h> //Serial library
 
+//software serial connection set up on pins 0 and 1
 SoftwareSerial bt (0,1);  //RX, TX (Switched on the Bluetooth - RX -> TX | TX -> RX)
 float fsr = 0;
 int inputPin = A5;
 int btdata; // the data given from the computer
  
 void setup() {
-  bt.begin(9600);
+  bt.begin(9600); //begin serial connection with 9600 baud rate
   pinMode(inputPin, INPUT);
 }
  
